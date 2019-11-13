c70bf8198f7ddfde2f683fa1f5e0376f2d85342f
Thomas Uher <thomas.uher@sisol-systems.com>
2019-11-13 15:31:24 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = u'LocalCosmos Private Server'
-copyright = u'2019, Thomas Uher'
+copyright = u'2019, SiSol Systems'
 author = u'Thomas Uher'
 
 # The short X.Y version
@@ -66,7 +66,7 @@ language = None
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -74,7 +74,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
